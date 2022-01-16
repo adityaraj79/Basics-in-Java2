19ceb6e6e32448bb88fa446aee40f68e12d3b391
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-01-16 20:42:17 +0530

    Update setBit.java

--- setBit.java
+++ setBit.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int pos = sc.nextInt();
+        int pos = sc.nextInt();             // setBit: to make the ith bit 1
         int bitmask = 1<<pos;
         int newNumber = (bitmask|n);
         System.out.println(newNumber);
