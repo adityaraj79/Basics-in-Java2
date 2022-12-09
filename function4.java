0d6bc2da6d43ca272ff55afb100830066aeff43b
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-12-09 23:54:37 +0530

    Create function4.java

---
+++ function4.java
@@ -0,0 +1,15 @@
+package com.basicsinjava;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        String name = "Aditya";
+        change(name);
+        System.out.println(name);
+    }
+
+    static void change(String naam){
+        naam = "Rahul";
+    }
+}
