3bfcf2d030a07008165f65b2fbe5a2f6d7c2e4ad
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-12-09 23:08:18 +0530

    Create function2.java

---
+++ function2.java
@@ -0,0 +1,15 @@
+package com.basicsinjava;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+        System.out.println(greet());
+        }
+    }
+
+    static String greet(){
+        return "Hello world!";
+    } 
+}
