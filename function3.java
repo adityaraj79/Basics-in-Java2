25cb76311d9cfa0dbff42e9f9dd851bba70f34f7
Aditya Raj <79632537+adityaraj79@users.noreply.github.com>
2022-12-09 23:56:42 +0530

    Update function3.java

--- function3.java
+++ function3.java
@@ -7,11 +7,12 @@ public class Main{
         int a = 10;
         int b = 20;
         swap(a,b);
+        System.out.println(a +" "+ b);
+
     } 
     static void swap(int x, int y){
         int temp = x;
         x = y;
         y = temp;
-        System.out.println(x +" "+ y);
     }
 }
